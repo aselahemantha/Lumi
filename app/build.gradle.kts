e1abd8e0065bf6ffddfd039be333d5171f48bb7a
Asela Hemantha <>
2026-08-17 06:46:48 +0530

    Bump application version code and version name
    - Increment `versionCode` to 6 and `versionName` to 1.2 in `app/build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.nebulatech.lumi"
         minSdk = 24
         targetSdk = 37
-        versionCode = 5
-        versionName = "1.1"
+        versionCode = 6
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
